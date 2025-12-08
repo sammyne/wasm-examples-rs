e250114f7da2774c6a7a5bab460d928ffb49471e
sammyne <xiangminli@alumni.sjtu.edu.cn>
2025-12-08 10:53:38 +0000

    feat(docker): add wac-cli

--- docker/dockerize.sh
+++ docker/dockerize.sh
@@ -8,6 +8,7 @@ repo_tag=sammyne/wasm-studio-rs:`git rev-parse --short HEAD`
 
 build_arg_opts="--build-arg CARGO_COMPONENT_VERSION=0.21.1"
 build_arg_opts="$build_arg_opts --build-arg MOLD_VERSION=2.40.4"
+build_arg_opts="$build_arg_opts --build-arg WAC_VERSION=0.8.1"
 build_arg_opts="$build_arg_opts --build-arg WASM_TOOLS_VERSION=1.243.0"
 build_arg_opts="$build_arg_opts --build-arg WASMTIME_VERSION=39.0.1"
 build_arg_opts="$build_arg_opts --build-arg WIT_BINDGEN_VERSION=0.49.0"
