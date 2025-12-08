ce47db771bce589e96695352660a86265aac8805
sammyne <xiangmin-li@qq.com>
2025-12-08 08:54:55 +0000

    feat(docker): upgrade wasmtime to v39

--- docker/dockerize.sh
+++ docker/dockerize.sh
@@ -6,12 +6,10 @@ cd `dirname ${BASH_SOURCE[0]}`
 
 repo_tag=sammyne/wasm-studio-rs:`git rev-parse --short HEAD`
 
-build_arg_opts="--build-arg CARGO_COMPONENT_VERSION=0.15.0"
-build_arg_opts="$build_arg_opts --build-arg GO_VERSION=1.23.0"
-build_arg_opts="$build_arg_opts --build-arg MOLD_VERSION=2.33.0"
-build_arg_opts="$build_arg_opts --build-arg TINYGO_VERSION=0.33.0"
-build_arg_opts="$build_arg_opts --build-arg WASM_TOOLS_VERSION=1.216.0"
-build_arg_opts="$build_arg_opts --build-arg WASMTIME_VERSION=20.0.0"
-build_arg_opts="$build_arg_opts --build-arg WIT_BINDGEN_VERSION=0.30.0"
+build_arg_opts="--build-arg CARGO_COMPONENT_VERSION=0.21.1"
+build_arg_opts="$build_arg_opts --build-arg MOLD_VERSION=2.40.4"
+build_arg_opts="$build_arg_opts --build-arg WASM_TOOLS_VERSION=1.243.0"
+build_arg_opts="$build_arg_opts --build-arg WASMTIME_VERSION=39.0.1"
+build_arg_opts="$build_arg_opts --build-arg WIT_BINDGEN_VERSION=0.49.0"
 
 docker build $build_arg_opts -t $repo_tag --progress plain .
